9d4a5d80e16ca2eb10816bfe4ce988752e87f327
JeGalJeong <60964490+jk0601@users.noreply.github.com>
2025-05-18 19:26:58 +0900

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    ppr: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'avatar.vercel.sh',
+      },
+    ],
+  },
+  output: 'standalone',
+};
+
+module.exports = nextConfig; 
